c85a24b1377c8308f9af74c6c93c0bf3266b60c6
lotestudio <info@lote.bg>
2026-04-22 19:25:51 +0300

    start of navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import {Folders, MenuSquareIcon} from 'lucide-vue-next'
+
+</script>
 
 <template>
   <div class="app-shell">
@@ -10,10 +13,27 @@
       </router-view>
     </main>
 
-    <nav class="app-nav flex justify-around p-4 shrink-0">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/catalog">Catalog</RouterLink>
-      <RouterLink to="/settings">Settings</RouterLink>
+    <nav class="app-nav flex justify-around px-1.5 py-2.5 shrink-0 text-sm">
+      <RouterLink to="/" active-class="text-gray-400" class="flex flex-col items-center gap-2">
+        <Folders class="size-6" />
+        <div>Matches</div>
+      </RouterLink>
+      <RouterLink to="/catalog" active-class="text-gray-400" class="flex flex-col items-center gap-2">
+        <Folders class="size-6" />
+        <div>News</div>
+      </RouterLink>
+      <RouterLink to="/settings" active-class="text-gray-400" class="flex flex-col items-center gap-2">
+        <Folders class="size-6" />
+        <div>Leagues</div>
+      </RouterLink>
+      <RouterLink to="/settings" active-class="text-gray-400" class="flex flex-col items-center gap-2">
+        <Folders class="size-6" />
+        <div>Following</div>
+      </RouterLink>
+      <RouterLink to="/settings" active-class="text-gray-400" class="flex flex-col items-center gap-2">
+        <MenuSquareIcon class="size-6" />
+        <div>More</div>
+      </RouterLink>
     </nav>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // vueDevTools(),
+    // vueDevTools({
+    //   launchEditor: 'phpstorm',
+    // }),
     tailwindcss(),
-    VitePWA({ registerType: 'autoUpdate' })
+    // VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
